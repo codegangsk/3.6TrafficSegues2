753430c3b16775bcff2090dce0b6c5f5a79fd37c
codegangsk <kjhj0604@gmail.com>
2020-08-13 17:00:20 +0900

    Initial Commit

---
+++ TrafficSegues(3.6 project)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues(3.6 project)
+//
+//  Created by Sophie Kim on 2020/08/13.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
