ef3d6e9c00e030d320ec38ee7b1193a5495f8737
codegangsk <kjhj0604@gmail.com>
2020-08-19 17:11:53 +0900

    first commit

--- TrafficSegues(3.6 project)/ViewController.swift
+++ TrafficSegues(3.6 project)/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segueSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        
+        if segueSwitch.isOn {performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
 }
 
